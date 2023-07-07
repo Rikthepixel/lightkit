e96a9760c3676f82a19935daeb277a746f919176
RikThePixel <Rikdenbreejen@hotmail.nl>
2023-07-07 21:39:22 +0200

    Add testing to the Minkit fetch package

---
+++ packages/fetch/src/modules/register.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, test } from "@jest/globals";
+import { HandlerRegister } from "./register";
+
+describe("HandlerRegister", () => {
+
+    test("Copy HandlerRegistry copies the internal maps/arrays", () => {
+        //
+        // Arrange
+        //
+
+        const register = new HandlerRegister();
+
+        //
+        // Act
+        //
+
+        const sut = register.copy();
+
+        //
+        // Assert
+        //
+
+        expect(sut.globalParser).toBe(register.globalParser);
+        expect(sut.parsers).not.toBe(register.parsers);
+    });
+
+});
